1b0f138d9d6e29fe5762ee1f53d953d230225754
Siraaj Khandkar <siraaj@khandkar.net>
2018-04-15 17:18:15 -0400

    Use green/black color scheme for status bar

--- config.h
+++ config.h
@@ -9,13 +9,16 @@ static const char *fonts[]          = { "terminus:size=10" };
 static const char dmenufont[]       = "terminus:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+/*static const char col_gray3[]       = "#bbbbbb";*/
+/*static const char col_gray4[]       = "#eeeeee";*/
+/*static const char col_cyan[]        = "#005577"; // original*/
+static const char col_green[]       = "#60b48a"; // zenburn
+/*static const char col_cyan[]        = "#9fafaf"; // zenburn - too-bland */
+/*static const char col_cyan[]        = "#9ab8d7";  // "Dark Pastels" - too-bright*/
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_green, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray1, col_green,  col_green  },
 };
 
 /* tagging */
@@ -56,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_green, "-sb", col_green, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "konsole", NULL };
 
 static Key keys[] = {
