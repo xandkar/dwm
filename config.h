854b4f6aa93f38692658cbbf47f1adda69047cdf
Siraaj Khandkar <siraaj@khandkar.net>
2018-11-02 14:41:15 -0400

    Return to font 10

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "terminus:size=8:weight:bold" };
-static const char dmenufont[]       =   "terminus:size=8";
+static const char *fonts[]          = { "terminus:size=10:weight:bold" };
+static const char dmenufont[]       =   "terminus:size=10";
 
 /* TODO: Give all colors discrete names, instead of postfixing gradient */
 /*static const char col_gray1[]     = "#222222";*/
