87ee506e9a837ce0af2ac3e94d6cc419e869879c
Siraaj Khandkar <siraaj@khandkar.net>
2019-01-06 13:35:30 -0500

    Add rules for qutebrowser (replacing Firefox) and Spotify

--- config.h
+++ config.h
@@ -52,9 +52,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class         instance    title       tags mask     isfloating   monitor */
+	{ "Gimp"       , NULL,       NULL,       0     ,       1,           -1 },
+	{ "qutebrowser", NULL,       NULL,       1 << 8,       1,           -1 },
+	{ "Spotify"    , NULL,       NULL,       1 << 8,       1,           -1 },
 };
 
 /* layout(s) */
