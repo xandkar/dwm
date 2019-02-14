99885080b3eed29a76255ac14fc1241f041cd166
Siraaj Khandkar <siraaj@khandkar.net>
2019-02-14 05:58:42 -0500

    Revert "Apply alpha patch"
    This reverts commit cd1044c6536630dceba5932c497ee987952bdfd1.

--- config.def.h
+++ config.def.h
@@ -26,11 +26,6 @@ static const char *colors[][3]      = {
 	[SchemeWarn] =	 { col_black, col_yellow, col_red },
 	[SchemeUrgent]=	 { col_white, col_red,    col_red },
 };
-static const unsigned int alphas[][3]      = {
-	/*               fg      bg        border     */
-	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
-};
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
